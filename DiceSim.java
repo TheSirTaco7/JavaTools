103b514aac18b094d65a89e9be819fde0697171b
TheSirTaco7 <114195041+TheSirTaco7@users.noreply.github.com>
2023-07-24 13:28:04 -0400

    Got d20 Working

--- DiceSim.java
+++ DiceSim.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.Scanner;
 
 class DiceSim {
 
@@ -10,53 +9,44 @@ class DiceSim {
         String input = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (!exit) {            
-
-            System.out.println("\n\n\n\n\n\n\n\n\n\n\n============Dice Simulator============\n");
+        while (!exit) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n============Dice Simulator============\n");
             System.out.println("");
             System.out.println("What dice would you like to roll?\n");
             System.out.println("Enter 'd20', 'd12', 'd10', 'percentile', 'd8', 'd6', 'd4', or 'exit'\n");
 
             try {
-
                 input = br.readLine().trim();
-
             } catch (IOException ioe) {
                 // Do Nothing
             }
 
             if (input.equals("exit")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                System.out.println("Bye");
                 exit = true;
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             } else if (input.equals("d20")) {
-                try {
-                    rollD20();
-                } catch (IOException e) {
-                    System.err.println("An error occurred while rolling the dice: " + e.getMessage());
-                    exit = true; // Exit the loop in case of an error
-                }
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD20(); // Call the method to roll the d20 dice
             } else {
                 System.out.println("Enter 'd20', 'd12', 'd10', 'percentile', 'd8', 'd6', 'd4', or 'exit'\n");
             }
         }
     }
 
-    public static void rollD20() throws IOException {
-
+    public static void rollD20() {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
         int random_number = rand.nextInt(20) + 1; // Add 1 to get a number between 1 and 20
         System.out.println("\nThe Result is " + random_number + "\n");
-        System.out.print("Do you want to roll this dice again? (y/n)\n");
-        scanner.nextLine();
-        if (userInput.equals("n")) { // Check if the user entered 'n'
-            // Code will continue executing if the user entered 'n'
-            System.out.println("You have continued!"); // Print a message to the console
+        System.out.print("Do you want to roll this dice again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
         } else {
-            System.out.println("You did not enter 'n'. Program will not continue."); // Print a message if user input is not 'n'
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD20(); // Call the method recursively to roll the dice again
         }
-
-
     }
-    
 }
