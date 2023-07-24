3d8737d74ec3f88f30c9b2a3f3d4c41205c2a6be
TheSirTaco7 <114195041+TheSirTaco7@users.noreply.github.com>
2023-07-24 13:54:07 -0400

    Added The Rest of the die
    Added d12, d10, d8, d6, d4, and percentile and compiled Code.

--- DiceSim.java
+++ DiceSim.java
@@ -13,7 +13,7 @@ class DiceSim {
             System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n============Dice Simulator============\n");
             System.out.println("");
             System.out.println("What dice would you like to roll?\n");
-            System.out.println("Enter 'd20', 'd12', 'd10', 'percentile', 'd8', 'd6', 'd4', or 'exit'\n");
+            System.out.println("Enter 'd20', 'd12', 'd10', 'd8', 'd6', 'd4', 'percentile', or 'exit'\n");
 
             try {
                 input = br.readLine().trim();
@@ -29,6 +29,24 @@ class DiceSim {
             } else if (input.equals("d20")) {
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 rollD20(); // Call the method to roll the d20 dice
+            } else if (input.equals("d12")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD12(); // Call the method to roll the d12 dice
+            } else if (input.equals("d10")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD10(); // Call the method to roll the d10 dice
+            } else if (input.equals("d8")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD8(); // Call the method to roll the d8 dice
+            } else if (input.equals("d6")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD6(); // Call the method to roll the d6 dice
+            } else if (input.equals("d4")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollD4(); // Call the method to roll the d4 dice
+            } else if (input.equals("percentile")) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                rollpercentile(); // Call the method to roll the percentile dice
             } else {
                 System.out.println("Enter 'd20', 'd12', 'd10', 'percentile', 'd8', 'd6', 'd4', or 'exit'\n");
             }
@@ -40,13 +58,103 @@ class DiceSim {
         Random rand = new Random();
         int random_number = rand.nextInt(20) + 1; // Add 1 to get a number between 1 and 20
         System.out.println("\nThe Result is " + random_number + "\n");
-        System.out.print("Do you want to roll this dice again? (y/n)\n\n");
+        System.out.print("Do you want to roll d20 again? (y/n)\n\n");
         String userInput = scanner.nextLine(); // Read the user input
 
         if (userInput.equals("n")) {
         } else {
             System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
             rollD20(); // Call the method recursively to roll the dice again
+        }
+    }
+
+     public static void rollD12() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(12) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "\n");
+        System.out.print("Do you want to roll d12 again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD12(); // Call the method recursively to roll the dice again
+        }
+    }
+
+    public static void rollD10() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(10) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "\n");
+        System.out.print("Do you want to roll d10 again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD10(); // Call the method recursively to roll the dice again
+        }
+    }
+
+    public static void rollD8() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(8) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "\n");
+        System.out.print("Do you want to roll d8 again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD8(); // Call the method recursively to roll the dice again
+        }
+    }
+
+    public static void rollD6() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(6) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "\n");
+        System.out.print("Do you want to roll d6 again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD6(); // Call the method recursively to roll the dice again
+        }
+    }
+
+    public static void rollD4() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(4) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "\n");
+        System.out.print("Do you want to roll d4 again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollD4(); // Call the method recursively to roll the dice again
+        }
+    }
+
+    public static void rollpercentile() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int random_number = rand.nextInt(100) + 1; // Add 1 to get a number between 1 and 20
+        System.out.println("\nThe Result is " + random_number + "%\n");
+        System.out.print("Do you want to roll percentile again? (y/n)\n\n");
+        String userInput = scanner.nextLine(); // Read the user input
+
+        if (userInput.equals("n")) {
+        } else {
+            System.out.println("\nYou have decided to roll the dice again!\n\n\n\n\n\n\n");
+            rollpercentile(); // Call the method recursively to roll the dice again
         }
     }
 }
